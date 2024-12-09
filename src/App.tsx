8f69b389b48fa9fe0d693c1582cc2ee101b39ef9
9oodam <godamcity98@gmail.com>
2024-12-09 16:31:59 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ import { Button } from "./components/ui/button";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 // const socket = io("http://192.168.1.7:8080");
-const socket = io("http://192.168.1.7:8080", {
-  transports: ["websocket", "polling"], // polling도 허용
+const socket = io("ws://192.168.1.7:8080", {
+  transports: ["websocket"],
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
-  secure: false, // HTTP 연결 허용
-  rejectUnauthorized: false, // 인증서 검증 건너뛰기
+  secure: false,
+  rejectUnauthorized: false,
 });
 
 const App = () => {
@@ -25,6 +25,14 @@ const App = () => {
 
   // WebSocket 연결
   useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected to server");
+    });
+
+    socket.on("connect_error", (error) => {
+      console.error("Connection error:", error);
+    });
+
     // 초음파 센서 데이터 받기
     socket.on("ultrasonic_data", (data) => {
       console.log(data.distance);
